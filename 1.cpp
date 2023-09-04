7ccb780caa57ecc0b06c1d6bdada1b9dd3d70fb5
AKAD0 <onixon2010@gmail.com>
2023-09-04 12:47:20 +0300

    new file:   1.cpp

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    /*
+    //— hardcore strings:
+        //Strings can be defined both equivalent ways:
+        //char var5[4] = {'h','u','i','\0'};
+        //char var5[4] = "hui";
+
+    //Common str definition in principle contains following:
+    //char var5[4] = "hui";
+    //char* var6 = var5;
+
+    char* var7 = "hui";
+    printf("asdf %s", var7);
+    */
+
+    /*
+    //— handy strings:
+    std::string var5 = "ebat";
+    std::cout << var5 << std::endl;
+    */
+   
+    return 0;
+}
