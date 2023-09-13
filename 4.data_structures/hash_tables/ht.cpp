2e9fb7ccbddf9568d064c1371fe18db68f5cd490
AKAD0 <onixon2010@gmail.com>
2023-09-13 14:02:48 +0300

    renamed:    3.cycles-func-proc/func_proc/1.cpp -> 3.cycles-func-proc/func_proc/func_proc.cpp 	renamed:    3.cycles-func-proc/func_proc/1.exe -> 3.cycles-func-proc/func_proc/func_proc.exe 	modified:   4.data_structures/hash_tables/ht.cpp 	modified:   4.data_structures/hash_tables/ht.exe

--- 4.data_structures/hash_tables/ht.cpp
+++ 4.data_structures/hash_tables/ht.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-int hash_function(std::string hash_key){
-    int i, hash_key_size, hash_table_size, hash_val=0;
+//Definintion of hash function
+int hash_function(std::string hash_key, int hash_table_size){
+    int i, hash_key_size, hash_val=0;
     hash_key_size = hash_key.size();
-    hash_table_size = 10;
 
     for (i=0; i<hash_key_size; i++){
         hash_val+=hash_key[i];
@@ -12,11 +12,28 @@ int hash_function(std::string hash_key){
     return( hash_val);
 };
 
+//Definition of item in hash table
+typedef struct Ht_item
+{
+    std::string* key;
+    std::string* value;   
+} Ht_item;
+
+//Definition of Hash table itself
+typedef struct HashTable
+{
+    Ht_item** items; //'items' is a pointer to array of pointers where each of them are 'Ht_item' type.
+    int size;
+    int count;
+} HashTable;
+
+
 int main(){
     //hash function
-    std::string string="helloo";
+    int hash_table_size = 10;
+    std::string string="hello";
     int hash_key;
-    hash_key = hash_function(string);
+    hash_key = hash_function(string, hash_table_size);
     std::cout << hash_key << std::endl;
     return 0;
 };
