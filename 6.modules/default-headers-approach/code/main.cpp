b40ddf3844ffd858c3192f5c3aef295eb7c5198e
AKAD0 <onixon2010@gmail.com>
2023-09-17 16:22:24 +0300

    modified:   6.modules/default-headers-approach/code/main.cpp

--- 6.modules/default-headers-approach/code/main.cpp
+++ 6.modules/default-headers-approach/code/main.cpp
@@ -1,3 +1,10 @@
+//Building app includes following:
+//1. Compiling both modules and main program via:
+//   'g++ -c module.cpp' and
+//   'g++ -c main.cpp'
+//2. Linking resulting .o (object) files and buidling app via:
+//   'g++ main.o module.o -o App'
+
 #include "../headers/main.h"
 
 int main(){
