2e470fc3dec70f3c97f65ff3681b37d287eab251
AKAD0 <onixon2010@gmail.com>
2023-09-17 15:09:47 +0300

    modified:   module.cxx

--- 6.modules/module.cxx
+++ 6.modules/module.cxx
@@ -1,3 +1,10 @@
+//Building app includes following:
+//1. Compiling both modules and main program via:
+//   'g++ -c -std=c++20 -fmodules-ts module.cxx' and
+//   'g++ -c -std=c++20 -fmodules-ts main.cpp'
+//2. Linking resulting .o (object) files and buidling app via:
+//   'g++ main.o module.o -o App'
+
 // module.cxx
 module;
 
